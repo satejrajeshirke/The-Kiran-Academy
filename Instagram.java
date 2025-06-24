3a903e3bfa26377f21b347a4ee7687d17900629c
satejrajeshirke <satejrajeshirke007@gmail.com>
2025-06-24 08:54:30 +0530

    Instagram.java

---
+++ Instagram.java
@@ -0,0 +1,10 @@
+
+public class Instagram {
+
+	
+	void LoginCheck(String username,String password) {
+		
+		System.out.println("Username : "+username);
+		System.out.println("password : "+password);
+	}
+}
