93fbe7d54679ca06eaf34b4700e0556fd4170f4c
satejrajeshirke <satejrajeshirke007@gmail.com>
2025-07-04 00:39:03 +0530

    TestDefault.java

---
+++ TestDefault.java
@@ -0,0 +1,24 @@
+package com.tka.july3;
+
+public class TestDefault {
+
+	
+	public static void main(String args[]) {
+		DefaultValue var=new DefaultValue();
+		
+		System.out.println(var.i);
+		System.out.println(var.b);
+		System.out.println(var.f);
+		System.out.println(var.b1);
+		System.out.println(var.s);
+		System.out.println(var.l);
+		System.out.println(var.c);
+		System.out.println(var.b2);
+		System.out.println(var.str);
+
+
+
+		
+	   
+	}
+}
