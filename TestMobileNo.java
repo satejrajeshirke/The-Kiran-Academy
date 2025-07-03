277d61cb66a66a82411f7b23194358ef8a2e6e42
satejrajeshirke <satejrajeshirke007@gmail.com>
2025-07-04 00:39:40 +0530

    TestMobileNo.java

---
+++ TestMobileNo.java
@@ -0,0 +1,13 @@
+package com.tka.july3;
+
+public class TestMobileNo {
+
+	
+	public static void main(String args[]) {
+		
+		MobileNo m=new MobileNo();
+		  System.out.println(m.a);
+		  
+		  
+	}
+}
